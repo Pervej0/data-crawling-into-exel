158209b8f4fee38be8d3e04ad0fc1938a7c342b1
Pervej0 <mdparvez222khan@gmail.com>
2024-08-17 10:12:08 +0600

    updated

--- devices.js
+++ devices.js
@@ -1,30 +1,15 @@
 const fs = require("fs");
 const XLSX = require("xlsx");
+require("dotenv").config();
 let allData = [];
 
-// const detailsUrl = "https://efdms.nbr.gov.bd/dashboard/device/view/20866681";
-
-// const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6YW1hbiIsImlhdCI6MTcyMzcxMzQ2MCwiZXhwIjoxNzIzNzk5ODYwfQ.yBCgBXDG7ey8kjpgpMLxLas1PWTB45d3sdWj100Uxp-MNV7c5Ro8YzOV8tHVJ6K9oLlXq-lyZ5xebn7QA1lxQQ";
-
-// fetch(detailsUrl, {
-//   method: "GET",
-//   headers: {
-//     Authorization: `Bearer ${token}`,
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((error) => console.error("Error:", error));
-
 const devicesCount = () => {
-  for (let i = 1; i < 440; i++) {
+  let url;
+  const token = process.env.TOK;
+  console.log(token, process.env.URL, "ccc");
+  for (let i = 1; i < 2; i++) {
     const ConvertNum = String(i);
-    const url = `https://efdmsapi.nbr.gov.bd/efdms/services/api/inventory/devices?page=${ConvertNum}&&size=50`;
-    const token =
-      "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6YW1hbiIsImlhdCI6MTcyMzcxMzQ2MCwiZXhwIjoxNzIzNzk5ODYwfQ.yBCgBXDG7ey8kjpgpMLxLas1PWTB45d3sdWj100Uxp-MNV7c5Ro8YzOV8tHVJ6K9oLlXq-lyZ5xebn7QA1lxQQ";
+    url = `${process.env.URL}/services/api/inventory/devices?page=${ConvertNum}&&size=50`;
 
     fetch(url, {
       method: "GET",
@@ -34,41 +19,44 @@ const devicesCount = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        allData.push(data.content);
-        console.log(data.content.length, i);
+        allData.push(data);
+        console.log(data.content, i);
         // console.log(data.content);
         // console.log(i);
       })
       .catch((error) => console.error("Error:", error));
   }
-
+  if (allData.length < 1) {
+    return;
+  }
   setTimeout(() => {
-    // const jsonString = JSON.stringify(allData, null, 2);
-    // fs.writeFile("data.json", jsonString, (err) => {
-    //   if (err) {
-    //     console.error("Error writing JSON to file", err);
-    //   } else {
-    //     console.log("JSON file has been created successfully.");
-    //   }
-    // });
+    // json formatted data
+    const jsonString = JSON.stringify(allData, null, 2);
+    fs.writeFile("small-data.json", jsonString, (err) => {
+      if (err) {
+        console.error("Error writing JSON to file", err);
+      } else {
+        console.log("JSON file has been created successfully.");
+      }
+    });
 
-    if (allData.length > 0) {
-      const flattenedData = allData.flat();
+    // convert to excel
+    // if (allData.length > 0) {
+    //   const flattenedData = allData.flat();
 
-      // Convert the flattened array to a worksheet
-      const worksheet = XLSX.utils.json_to_sheet(flattenedData);
+    //   // Convert the flattened array to a worksheet
+    //   const worksheet = XLSX.utils.json_to_sheet(flattenedData);
 
-      // Create a new workbook
-      const workbook = XLSX.utils.book_new();
+    //   // Create a new workbook
+    //   const workbook = XLSX.utils.book_new();
 
-      // Append the worksheet to the workbook
-      XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    //   // Append the worksheet to the workbook
+    //   XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
 
-      // Export the Excel file
-      XLSX.writeFile(workbook, "nested_data.xlsx");
-    }
-    // console.log(allData);
+    //   // Export the Excel file
+    //   XLSX.writeFile(workbook, "nested_data.xlsx");
+    // }
   }, 20000);
 };
 
-// devicesCount();
+devicesCount();
