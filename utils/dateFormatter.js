b01b0c4ab12fa481f62b02e4aadd831e99be27df
Pervej0 <mdparvez222khan@gmail.com>
2024-08-22 10:55:40 +0600

    dateFormatter added

--- utils/dateFormatter.js
+++ utils/dateFormatter.js
@@ -1,10 +1,9 @@
-const dateFormate = () => {
+const dateFormatter = () => {
   const date = new Date();
 
   const year = String(date.getFullYear()).slice(-2);
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   const formattedDate = `${year}-${month}-${day}`;
-
   return formattedDate;
 };
